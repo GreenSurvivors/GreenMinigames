7abbaf437021a3a7752f1caccbdf51f0c2d9be48
R411gun <herobrine432@hotmail.de>
2023-04-03 18:50:56 +0200

    Update FinishSign.java

--- Minigames/src/main/java/au/com/mineauz/minigames/signs/FinishSign.java
+++ Minigames/src/main/java/au/com/mineauz/minigames/signs/FinishSign.java
@@ -52,7 +52,7 @@ public class FinishSign implements MinigameSign {
         if (!event.getLine(2).isEmpty() && plugin.getMinigameManager().hasMinigame(event.getLine(2))) {
             event.setLine(2, plugin.getMinigameManager().getMinigame(event.getLine(2)).getName(false));
         } else if (!event.getLine(2).isEmpty()) {
-            event.getPlayer().sendMessage(ChatColor.RED + "[Minigames] " + ChatColor.WHITE + MinigameUtils.getLang("minigame.error.noMinigame"));
+            event.getPlayer().sendMessage(ChatColor.RED + "§8§8⌈§x§1§9§f§b§0§0M§x§1§6§f§b§1§fi§x§1§3§f§c§3§en§x§1§0§f§c§5§di§x§0§d§f§c§7§cg§x§0§9§f§c§9§ba§x§0§6§f§d§b§am§x§0§3§f§d§d§9e§x§0§0§f§d§f§8s§8⌋ " + ChatColor.WHITE + MinigameUtils.getLang("minigame.error.noMinigame"));
             return false;
         }
         return true;
