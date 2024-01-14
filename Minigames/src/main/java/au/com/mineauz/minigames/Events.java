6090747adb5bcba0a01a46a6e7211c30f2c830df
FireInstall <kettnerl@hu-berlin.de>
2024-01-14 17:30:02 +0100

    bit of code maturity

--- Minigames/src/main/java/au/com/mineauz/minigames/Events.java
+++ Minigames/src/main/java/au/com/mineauz/minigames/Events.java
@@ -240,15 +240,6 @@ public class Events implements Listener {
                     if (mgm.getMinigameTimer() != null) mgm.getMinigameTimer().startTimer();
                 }
             }
-            //for(String mgm : mdata.getAllMinigames().keySet()){
-            //    if(mdata.getMinigame(mgm).getType() == MinigameType.GLOBAL){
-//                    if(minigameManager.getMinigame(mgm).getThTimer() != null){
-//                        minigameManager.getMinigame(mgm).getThTimer().pauseTimer(false);
-//                    }
-            //        if(mdata.getMinigame(mgm).getMinigameTimer() != null)
-            //            mdata.getMinigame(mgm).getMinigameTimer().startTimer();
-            //    }
-            //       }
         }
     }
 
@@ -469,7 +460,6 @@ public class Events implements Listener {
                     if (sb.getShooter() instanceof Player player) {
                         MinigamePlayer shooter = pdata.getMinigamePlayer(player);
                         Minigame mgm = mgPlayer.getMinigame();
-                        if (shooter == null) return;
                         if (shooter.isInMinigame() && shooter.getMinigame().equals(mgPlayer.getMinigame())) {
                             if (!shooter.canPvP()) {
                                 event.setCancelled(true);
