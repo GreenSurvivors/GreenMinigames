AddstarMC/Minigames

added Minigame field to Regions and nodes to get access to it without a player

Regions/src/main/java/au/com/mineauz/minigamesregions/NodeToolMode.java
@@ -103,7 +103,7 @@ public class NodeToolMode implements ToolMode {
             Location loc = event.getClickedBlock().getLocation().add(0.5, 0.5, 0.5);
             Node node = mod.getNode(name);
             if (node == null) {
-                node = new Node(name, loc);
+                node = new Node(name, minigame, loc);
                 mod.addNode(name, node);
                 player.sendInfoMessage("Added new node to " + minigame + " called " + name);
             } else {
@@ -121,7 +121,7 @@ public class NodeToolMode implements ToolMode {
 
         Node node = mod.getNode(name);
         if (node == null) {
-            node = new Node(name, player.getLocation());
+            node = new Node(name, minigame, player.getLocation());
             mod.addNode(name, node);
             player.sendInfoMessage("Added new node to " + minigame + " called " + name);
         } else {

Regions/src/main/java/au/com/mineauz/minigamesregions/RegionToolMode.java
@@ -104,7 +104,7 @@ public class RegionToolMode implements ToolMode {
             Region region = module.getRegion(name);
 
             if (region == null) {
-                module.addRegion(name, new Region(name, player.getSelectionPoints()[0], player.getSelectionPoints()[1]));
+                module.addRegion(name, new Region(name, minigame, player.getSelectionPoints()[0], player.getSelectionPoints()[1]));
                 player.sendInfoMessage("Created a new region in " + minigame + " called " + name);
                 player.clearSelection();
             } else {

Regions/src/main/java/au/com/mineauz/minigamesregions/commands/SetNodeCommand.java
@@ -68,7 +68,7 @@ public class SetNodeCommand implements ICommand {
             RegionModule rmod = RegionModule.getMinigameModule(minigame);
             if (args[0].equalsIgnoreCase("create") && args.length >= 2) {
                 if (!rmod.hasNode(args[1])) {
-                    rmod.addNode(args[1], new Node(args[1], ply.getLocation()));
+                    rmod.addNode(args[1], new Node(args[1], minigame, ply.getLocation()));
                     sender.sendMessage(ChatColor.GRAY + Main.getPlugin().getMessage("command.node.addedNode", args[1], minigame.getName(true)));
                 } else
                     sender.sendMessage(ChatColor.RED + Main.getPlugin().getMessage("command.node.nodeExists", args[1], minigame.getName(true)));

Regions/src/main/java/au/com/mineauz/minigamesregions/commands/SetRegionCommand.java
@@ -90,7 +90,7 @@ public class SetRegionCommand implements ICommand {
                 } else if (args[0].equalsIgnoreCase("create")) {
                     if (ply.hasSelection()) {
                         String name = args[1];
-                        rmod.addRegion(name, new Region(name, ply.getSelectionPoints()[0], ply.getSelectionPoints()[1]));
+                        rmod.addRegion(name, new Region(name, minigame, ply.getSelectionPoints()[0], ply.getSelectionPoints()[1]));
                         ply.clearSelection();
 
                         ply.sendInfoMessage(ChatColor.GRAY + "Created new region for " + minigame.getName(false) + " named " + name);

GreenSurvivors/GreenMinigames

Regions/src/main/java/au/com/mineauz/minigamesregions/NodeToolMode.java
@@ -112,7 +112,7 @@ public class NodeToolMode implements ToolMode {
             Location loc = event.getClickedBlock().getLocation().add(0.5, 0.5, 0.5);
             Node node = mod.getNode(name);
             if (node == null) {
-                node = new Node(name, loc);
+                node = new Node(name, minigame, loc);
                 mod.addNode(name, node);
                 MinigameMessageManager.sendMessage(mgPlayer, MinigameMessageType.INFO, RegionMessageManager.getBundleKey(),
                         RegionLangKey.NODE_ADDED,
@@ -136,7 +136,7 @@ public class NodeToolMode implements ToolMode {
 
         Node node = mod.getNode(name);
         if (node == null) {
-            node = new Node(name, mgPlayer.getLocation());
+            node = new Node(name, minigame, mgPlayer.getLocation());
             mod.addNode(name, node);
             MinigameMessageManager.sendMessage(mgPlayer, MinigameMessageType.INFO, RegionMessageManager.getBundleKey(),
                     RegionLangKey.NODE_ADDED,

Regions/src/main/java/au/com/mineauz/minigamesregions/RegionToolMode.java
@@ -111,7 +111,7 @@ public class RegionToolMode implements ToolMode {
             Region region = module.getRegion(name);
 
             if (region == null) {
-                module.addRegion(name, new Region(name, mgPlayer.getSelectionLocations()[0], mgPlayer.getSelectionLocations()[1]));
+                module.addRegion(name, new Region(name, minigame, mgPlayer.getSelectionLocations()[0], mgPlayer.getSelectionLocations()[1]));
                 MinigameMessageManager.sendMessage(mgPlayer, MinigameMessageType.INFO, RegionMessageManager.getBundleKey(),
                         RegionLangKey.REGION_CREATED,
                         Placeholder.unparsed(MinigamePlaceHolderKey.MINIGAME.getKey(), minigame.getName(false)),

Regions/src/main/java/au/com/mineauz/minigamesregions/commands/SetNodeCommand.java
@@ -73,7 +73,7 @@ public class SetNodeCommand implements ICommand {
             RegionModule rmod = RegionModule.getMinigameModule(minigame);
             if (args[0].equalsIgnoreCase("create") && args.length >= 2) {
                 if (!rmod.hasNode(args[1])) {
-                    rmod.addNode(args[1], new Node(args[1], mgPlayer.getLocation()));
+                    rmod.addNode(args[1], new Node(args[1], minigame, mgPlayer.getLocation()));
                     MinigameMessageManager.sendMessage(sender, MinigameMessageType.INFO, RegionMessageManager.getBundleKey(),
                             RegionLangKey.NODE_ADDED,
                             Placeholder.unparsed(RegionPlaceHolderKey.NODE.getKey(), args[1]),

Regions/src/main/java/au/com/mineauz/minigamesregions/commands/SetRegionCommand.java
@@ -95,7 +95,7 @@ public class SetRegionCommand implements ICommand {
                 } else if (args[0].equalsIgnoreCase("create")) {
                     if (mgPlayer.hasSelection()) {
                         String name = args[1];
-                        rmod.addRegion(name, new Region(name, mgPlayer.getSelectionLocations()[0], mgPlayer.getSelectionLocations()[1]));
+                        rmod.addRegion(name, new Region(name, minigame, mgPlayer.getSelectionLocations()[0], mgPlayer.getSelectionLocations()[1]));
                         mgPlayer.clearSelection();
 
                         MinigameMessageManager.sendMessage(mgPlayer, MinigameMessageType.INFO, RegionMessageManager.getBundleKey(),